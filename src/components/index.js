65dbd5cb0a84005165089c65b1ab869c7d3cfeee
Chirag2001op <chirag.dogra79@gmail.com>
2022-03-18 23:36:04 +0530

    created opt-form

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-export {default as Accordion} from './accordion/index'
-export {default as Jumbotron} from './jumbotron/index';
-export {default as Footer} from './footer/index'
+export {default as Accordion} from './accordion'
+export {default as Jumbotron} from './jumbotron';
+export {default as Footer} from './footer'
+export {default as OptForm} from './opt-form'
